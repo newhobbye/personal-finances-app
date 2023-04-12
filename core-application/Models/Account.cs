6e77565d0e583fe04bfe866124d24aeb682de0e6
Robson Monteiro <rmonteiro@brasilcash.com.br>
2023-04-12 17:11:57 -0300

    Thinking about modeling

---
+++ core-application/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace core_application.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public int Code { get; set; }
+        public double Balance { get; set; }
+        public List<OldBalance> OldBalances { get; set; }
+
+    }
+}
---
+++ core-application/Models/OldBalance.cs
@@ -0,0 +1,11 @@
+﻿namespace core_application.Models
+{
+    public class OldBalance
+    {
+        public Guid Id { get; set; }
+        public int CodeAccount { get; set; }
+        public double OldBalanceValue { get; set; }
+        public DateTime BalanceDate { get; set; }
+        public List<double> OldBalances { get; set; }
+    }
+}
